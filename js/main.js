abf9d4077d2ded755432059878358cf9b02d78e8
Heinz Naumann <hlanaumann@gmail.com>
2021-11-17 13:30:32 +0100

    se añaden cambios varios para finalizar el proyecto

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
-
 jQuery(function () {
+  // Window load function
 
-	// Window load function
+  $(window).load(function () {
+    //Open street  Map
+    var mapcanvas = document.getElementById("map-canvas");
+    if (mapcanvas) {
+      // Element exists
+      var coord = [39.572025, 3.217715]; // <--- coordinates here
 
-	$(window).load(function () {
+      var map = L.map("map-canvas", { scrollWheelZoom: false }).setView(
+        coord,
+        18,
+      );
 
-		//Open street  Map
-		var mapcanvas = document.getElementById("map-canvas");
-		if (mapcanvas) {
-			// Element exists
-			var coord = [39.57226778005461, 3.21869090673715]; // <--- coordinates here
+      L.tileLayer(
+        "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+        {},
+      ).addTo(map);
 
-			var map = L.map('map-canvas', { scrollWheelZoom: false }).setView(coord, 18);
+      var customIcon = L.icon({
+        iconUrl: "images/marker_1.png",
+        iconSize: [80, 95], // size of the icon
+        iconAnchor: [39, 78], // point of the icon which will correspond to marker's location
+      });
 
-			L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-
-			}).addTo(map);
-
-			var customIcon = L.icon({
-				iconUrl: 'images/marker_1.png',
-				iconSize: [80, 95], // size of the icon
-				iconAnchor: [40, 94] // point of the icon which will correspond to marker's location
-			});
-
-			var marker = L.marker(coord, { icon: customIcon }).addTo(map);
-
-		}
-	})
-
+      var marker = L.marker(coord, { icon: customIcon }).addTo(map);
+      const url =
+        "https://www.google.com/maps/place/Muebles+Miguel+Sureda/@39.5720197,3.2177146,15z/data=!4m2!3m1!1s0x0:0x74a2d4b50a5c8481?sa=X&ved=2ahUKEwi_-f2I9sLpAhX9AWMBHSX3CswQ_BIwCHoECA4QCA";
+      map.on("click", e => {
+        window.open(url);
+      });
+    }
+  });
 });
-
